e4b7e2fe0702cbb7f87c6dcdbd9a26c0d8c5f018
Aaron Roh <roharon98@gmail.com>
2023-11-25 02:20:33 +0000

    Modify: postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,6 @@
 ﻿pip3 install --user -r requirements.txt && az extension add --name functionapp
 
-sudo apt-get update && sudo apt-get install azure-functions-core-tools-4
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install azure-functions-core-tools-4 -y
